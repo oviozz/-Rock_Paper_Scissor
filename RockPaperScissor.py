03bd8dc9f7e9cb9b52da16fd73ce01276d3a061b
oviozz <42685801+oviozz@users.noreply.github.com>
2021-12-15 23:53:50 -0800

    Create RockPaperScissor.py

---
+++ RockPaperScissor.py
@@ -0,0 +1,106 @@
+import random
+you_Score = 0
+bot_Score = 0
+print("********************************")
+print(" WELCOME TO ROCK,PAPER,SCISSORS")
+
+number = random.randint(0,3)
+list = ["Rock","Paper","Scissor"]
+print("Choose One:",", ".join(list))
+
+
+Playagain = "yes"
+while Playagain == "yes":
+    bot = random.choice(list)
+    user = input("Pick One: ").capitalize()
+
+    while user not in list:
+        print("Invalid Response")
+        user = input("Pick One: ").capitalize()
+
+    #if user and bot ties
+    if user == bot:
+        print("You Both Tied")
+        print("Bot Picked:", bot)
+        you_Score+=1
+        bot_Score+=1
+        print("___________________________")
+        print("Your Score:",you_Score)
+        print("Bot Score:", bot_Score)
+        print("___________________________")
+
+    #if user win win
+    elif user == "Rock" and bot == "Scissor":
+        print("You Won")
+        print("Bot Picked:", bot)
+        you_Score+=1
+        print("___________________________")
+        print("Your Score:", you_Score)
+        print("Bot Score:", bot_Score)
+        print("___________________________")
+
+
+    elif user == "Paper" and bot == "Rock":
+        print("You Won")
+        print("Bot Picked:", bot)
+        you_Score+=1
+        print("___________________________")
+        print("Your Score:", you_Score)
+        print("Bot Score:", bot_Score)
+        print("___________________________")
+
+
+    elif user == "Scissor" and bot == "Paper":
+        print("You Won")
+        print("Bot Picked:", bot)
+        you_Score+=1
+        print("___________________________")
+        print("Your Score:", you_Score)
+        print("Bot Score:", bot_Score)
+        print("___________________________")
+
+
+    #if bot wins
+
+    elif user == "Scissor" and bot == "Rock":
+        print("You Lost")
+        print("Bot Picked:", bot)
+        bot_Score+=1
+        print("___________________________")
+        print("Your Score:", you_Score)
+        print("Bot Score:", bot_Score)
+        print("___________________________")
+
+
+    elif user == "Rock" and bot == "Paper":
+        print("You Lost")
+        print("Bot Picked:", bot)
+        bot_Score+=1
+        print("___________________________")
+        print("Your Score:", you_Score)
+        print("Bot Score:", bot_Score)
+        print("___________________________")
+
+
+    elif user == "Paper" and bot == "Scissor":
+        print("You Lost")
+        print("Bot Picked:", bot)
+        bot_Score+=1
+        print("___________________________")
+        print("Your Score:", you_Score)
+        print("Bot Score:", bot_Score)
+        print("___________________________")
+
+    Playagain = input("Do you want to play again: yes/no: ").lower()
+
+    if Playagain == "no":
+        if you_Score == bot_Score:
+            print("****************You Tied!****************")
+        if you_Score > bot_Score:
+            print("****************You Win!****************")
+        if you_Score < bot_Score:
+            print("****************You Win!****************")
+
+        print("**********Thanks for playing!***********")
+        print("**********Created by Prazwal************")
+        exit()
